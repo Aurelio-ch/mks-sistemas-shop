813df77ffa8cc9a05dff0b6a79c8beecebda6869
Aurelio Chagas <aurelio.chagas.c@gmail.com>
2023-11-14 15:34:27 -0300

    Home and product components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,43 @@
-import { Container } from './styles'
+'use client'
+import { ShoppingBag } from 'lucide-react'
+import {
+  Container,
+  Product,
+  ProductContainer,
+  ProductDetails,
+  ProductImage,
+} from './styles'
 
 export default function Home() {
+  const gerarProduct = [1, 2, 3, 4, 5, 6, 7, 8]
   return (
     <Container>
-      <div>
-        <h1>Ola</h1>
-      </div>
+      <ProductContainer>
+        {gerarProduct.map((product) => {
+          return (
+            <Product key={product}>
+              <ProductImage
+                src="/apple-watch.png"
+                width={500}
+                height={500}
+                alt=""
+                quality={100}
+              />
+              <ProductDetails>
+                <div>
+                  <h1>Apple Watch Series 4 GPS</h1>
+                  <span>R$399</span>
+                </div>
+                <p>Redesigned from scratch and completely revised. </p>
+              </ProductDetails>
+              <button>
+                <ShoppingBag />
+                Comprar
+              </button>
+            </Product>
+          )
+        })}
+      </ProductContainer>
     </Container>
   )
 }
--- src/app/(home)/styles.ts
+++ src/app/(home)/styles.ts
@@ -1,3 +1,84 @@
+import Image from 'next/image'
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.main`
+  min-height: calc(
+    100vh - 7.051875rem
+  ); // calculo para manter o conteudo sempre ocupado a tela toda
+  padding: 5rem 10rem;
+  background-color: #e5e5e5;
+`
+
+export const Product = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  border-radius: 8px;
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.13);
+  padding-top: 30px;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+
+    background-color: #0f52ba;
+    color: #fff;
+    border-style: none;
+
+    padding: 0.5rem;
+    font-size: 14px;
+    font-weight: 600;
+    transition: 0.3s;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #0c387c;
+    }
+  }
+`
+export const ProductImage = styled(Image)`
+  margin: 0 auto;
+  width: 110px;
+  height: auto;
+  padding-bottom: 10px;
+`
+export const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 20px 16px;
+  div {
+    display: flex;
+    align-items: start;
+    gap: 8px;
+    h1 {
+      font-size: 19px;
+      font-weight: 400;
+      color: #2c2c2c;
+    }
+
+    span {
+      font-size: 15px;
+      font-weight: bold;
+      color: #fff;
+      background-color: #373737;
+      padding: 6px 7px;
+      border-radius: 10px;
+    }
+  }
+
+  p {
+    font-size: 11px;
+    font-weight: 400;
+    color: #2c2c2c;
+  }
+`
+export const ProductContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 250px);
+  justify-content: center;
+  gap: 30px;
+`
