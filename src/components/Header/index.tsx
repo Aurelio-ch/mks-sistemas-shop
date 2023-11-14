b71ee3d46b525f8efb2e6ea8607979712d9f7322
Aurelio Chagas <aurelio.chagas.c@gmail.com>
2023-11-14 10:52:39 -0300

    add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { ShoppingCart } from 'lucide-react'
+import Image from 'next/image'
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Image
+        src="/logo.png"
+        width={200}
+        height={45.83}
+        alt="logo MKS Sistemas"
+      />
+      <div>
+        <ShoppingCart />
+        <span>0</span>
+      </div>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.5rem 4.2rem;
+  background-color: #0f52ba;
+
+  div {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    background-color: #fff;
+    padding: 0.5rem 1rem;
+    border-radius: 10px;
+    font-size: 1rem;
+    font-weight: bold;
+  }
+`
